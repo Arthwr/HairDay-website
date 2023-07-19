0cc238b2d8c3f43189b0692d1a9ed7cfc8dca4e9
Artuwo <artyr007@gmail.com>
2023-07-19 20:31:01 +0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// Listen for window resize events
+document.addEventListener("DOMContentLoaded", function () {
+  // Listen for window resize events
+  window.addEventListener("resize", function () {
+    let links = document.getElementById("menu");
+    let screenWidth =
+      window.innerWidth || this.document.documentElement.clientWidth;
+    if (screenWidth > 425) {
+      links.style.display = "flex";
+    }
+  });
+});
+
+// Toggle hamburger menu
+function toggleFunc() {
+  let links = document.getElementById("menu");
+
+  if (links.style.display === "flex") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "flex";
+  }
+}
